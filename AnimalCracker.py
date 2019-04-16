5a10be8877f5b97fb8b03bf346891feba54c89d1
gurudattvshenoy <40710154+gurudattvshenoy@users.noreply.github.com>
2019-04-16 13:16:10 +0530

    AnimalCracker coding problem

--- AnimalCracker.py
+++ AnimalCracker.py
@@ -1,1 +1,11 @@
+#ANIMAL CRACKERS: Write a function takes a two-word string and returns True
+#if both words begin with same letter
+#animal_crackers('Levelheaded Llama') --> True
+#animal_crackers('Crazy Kangaroo') --> False
 
+def animal_crackers(s):
+    res = s.lower().split()
+    return res[0][0] == res[1][0]
+
+print(animal_crackers('Levelheaded Llama'))
+print(animal_crackers('Crazy Kangaroo'))
